d97796b28ca60bf474d694ac812631b200ac7423
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-07-04 09:51:43 +0530

    build faild fix
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,11 +2,16 @@ import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { openapi } from '@/lib/source';
 import { APIPage } from 'fumadocs-openapi/ui';
+import { Callout } from 'fumadocs-ui/components/callout';
+import { Steps, Step } from 'fumadocs-ui/components/steps';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     APIPage: props => <APIPage {...openapi.getAPIPageProps(props)} />,
+    Callout,
+    Steps,
+    Step,
     ...components,
   };
 }
